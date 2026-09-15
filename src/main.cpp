01efca19b91e04f64ff68b947403463de10664dd
alanxie-1 <alanxie0926@gmail.com>
2026-09-14 20:13:08 -0400

    started transceive logic

--- src/main.cpp
+++ src/main.cpp
@@ -97,39 +97,135 @@ std::map<std::vector<uint8_t>, char> decodeDict = {
 
 
 std::map<char, std::vector<uint8_t>> encodeDict;
-void setup() {
+bool synchSent = false;
+bool stopReceived = false;
+std::vector<u_int8_t> readArray;
+int phaseEncoderVar;
+u_int8_t byte_buffer1;
+u_int8_t byte_buffer2;
+u_int8_t byte_buffer3;
 
+
+void setup() {
   Serial.begin(115200);
-    for (const auto& pair : decodeDict) {
+    for (const auto& pair : decodeDict) { //reverse dictionary setup
         encodeDict[pair.second] = pair.first;
     }
 }
 
 void loop() {
+  Serial.println("Are you reading or writing? Type READ or WRITE. \n");
+  if (Serial.available()) {
+    String state = Serial.readStringUntil('\n');
+    if (state == "READ") {
+      Serial.println("You selected read.");
+      if (synchSent == false) {
+        phaseDecoder(0x11);
+        phaseDecoder(0x11);
+        phaseDecoder(0x10); //sent synch!
+      }
+      //receive data...
+      while (stopReceived == false) {
+        float raw1 = analogRead(36);
+        float raw2 = analogRead(39);
+        float volt1 = raw1 * 3.3 / 4095.0;
+        float volt2 = raw2 * 3.3 / 4095.0;
+        byte_buffer1 = phaseEncoder(volt1, volt2);
+        float raw1 = analogRead(36);
+        float raw2 = analogRead(39);
+        byte_buffer2 = phaseEncoder(volt1, volt2);
+        float raw1 = analogRead(36);
+        float raw2 = analogRead(39);
+        byte_buffer3 = phaseEncoder(volt1, volt2);        
+          delay(500);
+        synchSent = true;
+      }
+    }
+
+  }
 
 }
 
 //Assuming the negative sign is already implemented into Q
-int phaseDecoder(u_int8_t phase, double I, double Q) {
+
+u_int8_t phaseEncoder(double I, double Q){
+  if ((I - Q) == 1.414) {
+    phaseEncoderVar = 45;
+  } else if ((I - Q) == 0) {
+    phaseEncoderVar = 135;
+  } else if ((I - Q) == -1.414) {
+    phaseEncoderVar == 225;
+  } else {
+    phaseEncoderVar = 315;
+  }
+  switch (phaseEncoderVar) {
+    case 45:
+      return 0x00;
+      break;
+
+    case 135:
+      return 0x01;
+      break;
+
+    case 225:
+      return 0x10;
+      break;
+
+    case 315:
+      return 0x11;
+      break;
+  }
+  return 0;
+}
+
+int I_PIN = 25;
+int Q_PIN = 26;
+double I;
+double Q;
+void phaseDecoder(u_int8_t phase) { //virtual 0 at 1.65
   switch (phase) {
     case 0x00: //45
-      I = 0.707;
-      Q = -0.707;
+      I = 2.357;
+      Q = 0.943;
+      dacWrite(25, I_PIN);
+      dacWrite(26, Q_PIN);
+      delay(500);
+      dacWrite(25, 1.65);
+      dacWrite(26, 1.65);
+      delay(500);
       break;
 
     case 0x01: //135
-      I = -0.707;
-      Q = -0.707;
+      I = 0.943;
+      Q = 0.943;
+      dacWrite(25, I_PIN);
+      dacWrite(26, Q_PIN);
+      delay(500);
+      dacWrite(25, 1.65);
+      dacWrite(26, 1.65);
+      delay(500);
       break;
 
     case 0x10: //225
-      I = -0.707;
-      Q = 0.707;
+      I = 0.943;
+      Q = 2.357;
+      dacWrite(25, I_PIN);
+      dacWrite(26, Q_PIN);
+      delay(500);
+      dacWrite(25, 1.65);
+      dacWrite(26, 1.65);
+      delay(500);
       break;
 
     case 0x11: //315
-      I = 0.707;
-      Q = 0.707;
+      I = 2.357;
+      Q = 2.357;
+      dacWrite(25, I_PIN);
+      dacWrite(26, Q_PIN);
+      delay(500);
+      dacWrite(25, 1.65);
+      dacWrite(26, 1.65);
+      delay(500);
       break;
   }
 }
